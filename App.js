fe9f8af6c21c8757a87aa610a3575519456a29d5
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-02-02 15:11:13 +0530

    Day -1

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <Text>Hello World!</Text> 
+      <Hello name="WEB"/>
+      <Text>Count : {count}</Text>
+      <Button onPress={()=>setCount(count+1)} title="Click Me" />
+      <StatusBar style="dark" /> 
     </View>
   );
 }
 
+
+const Hello=({name})=>{
+  return <Text>IM MOHAMMED {name}</Text>
+
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
